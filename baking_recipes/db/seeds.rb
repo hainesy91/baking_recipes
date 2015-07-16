6e02fcf8aaed76eb9a6eb13edb62c0418ed4be30
Christopher Haines <hainesy91@gmail.com>
2015-07-16 18:34:25 +0100

    added description on seeds

--- baking_recipes/db/seeds.rb
+++ baking_recipes/db/seeds.rb
@@ -8,7 +8,12 @@
 Recipe.delete_all
 Ingredient.delete_all
 
-r1 = Recipe.create(name: 'Vanilla Cheesecake', description: 'Classic American Style Cheesecake', time: '30 Mins', method: 'Step 1')
+r1 = Recipe.create(name: 'Vanilla Cheesecake', description: 'Classic American Style Cheesecake', time: '45 Mins', method: 'Melt the butter in a small bowl in the microwave for 1 minute/2 minutes (depending on wattage of your microwave).
+In a seperate bowl, place the brown sugar and the crushed digestive biscuits. Stir in the butter gradually until it gains a consistency similar to wet sand.
+Line a sponge tin with greaseproof paper, and spoon the biscuit base mixture into the tin, smoothing out so it covers the whole of the tin. Chill for 30 minutes.
+Whilst the biscuit base is chilling, beat together the cream cheese, vanilla extract and caster sugar in a bowl, gradually folding in the creme fraiche. It should be of a thick, dropping consistency in nature.
+Once the biscuit base has chilled, spoon over the filling, and smooth out with a knife.
+Leave to chill in the fridge for a further two hours and serve.')
 r2 = Recipe.create(name: 'Strawberry Cheesecake', description: 'Perfect for Summer', time: '40 mins', method: 'Step 1, Step 2')
 r3 = Recipe.create(name: 'Chocolate Cheesecake', description: 'Modern twist on an Original', time: '35 mins', method: 'Step 1')
 
@@ -25,7 +30,26 @@ i9 = Ingredient.create(name: 'Cherries')
 i10 = Ingredient.create(name: 'Cream')
 i11 = Ingredient.create(name: 'Baking Powder')
 i12 = Ingredient.create(name: 'Salt')
+i13 = Ingredient.create(name: 'Digestives')
+i14 = Ingredient.create(name: 'Cocoa')
 
 r1.ingredients_recipes.create(ingredient_id: i1.id)
 r1.ingredients_recipes.create(ingredient_id: i2.id)
+r1.ingredients_recipes.create(ingredient_id: i3.id)
+r1.ingredients_recipes.create(ingredient_id: i6.id)
+r1.ingredients_recipes.create(ingredient_id: i10.id)
 r2.ingredients_recipes.create(ingredient_id: i3.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
